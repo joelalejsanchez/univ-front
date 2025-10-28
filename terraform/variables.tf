af521bfd4a5c973146b491e6f15dae40d8009829
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-10-28 12:26:35 -0400

    cambios

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key" {}
+variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
 variable "subnet_id" {}
